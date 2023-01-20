7e9e6c1898eb2521ba02bdd7b1f4554a546a97c9
Bill Horvath <bhorvath@umich.edu>
2023-01-20 16:46:00 -0500

    Added rounding to two decimal places for per-minute cost calculations, and built out more robust unit tests.

--- src/co/deability/um_mcc/cost_calulator.py
+++ src/co/deability/um_mcc/cost_calulator.py
@@ -1,15 +1,39 @@
+import decimal
 from decimal import Decimal
-from typing import Final, List
+from typing import Any, Final, List
 
-MINUTES_PER_YEAR: Final[Decimal] = Decimal("2080") * Decimal("60")
+minutes_per_year: int = 2080 * 60
+MINUTES_PER_YEAR: Final[Decimal] = Decimal(str(minutes_per_year) + ".00")
+
+
+def _convert_to_two_dig_dec(value: Any) -> Decimal:
+    if isinstance(value, str):
+        value = "".join(value.split(","))
+        if "." not in value:
+            value += ".00"
+    elif isinstance(value, int):
+        value = str(value) + ".00"
+    elif isinstance(value, Decimal):
+        try:
+            value = round(value, 2)
+        except decimal.InvalidOperation:
+            ...
+        return value
+    else:
+        assert False
+    return Decimal(value)
 
 
 def calc_per_min_cost(salary: Decimal) -> Decimal:
-    return salary / MINUTES_PER_YEAR
+    salary: Decimal = _convert_to_two_dig_dec(salary)
+    per_min: Decimal = salary / MINUTES_PER_YEAR
+    rounded: Decimal = round(per_min, 2)
+    return rounded
 
 
 def calc_attendee_cost(salary: Decimal, minutes: int) -> Decimal:
-    return calc_per_min_cost(salary=salary) * minutes
+    attendee_cost: Decimal = calc_per_min_cost(salary=salary) * minutes
+    return attendee_cost
 
 
 def calc_meeting_cost(salaries: List[Decimal], minutes: int) -> Decimal:
--- test/test_core_functionality.py
+++ test/test_core_functionality.py
@@ -2,7 +2,11 @@ from decimal import Decimal
 
 from co.deability.um_mcc import person_finder, cost_calulator
 
-DOLLAR_PER_MIN_SALARY = Decimal("124800")
+minutes_per_year = int(60 * 2080)
+MINUTES_PER_YEAR = Decimal(str(minutes_per_year) + ".00")
+DOLLAR_PER_MIN_SALARY = MINUTES_PER_YEAR
+FIFTY_SALARY = Decimal("50000.00")
+WEIRD_SALARY = Decimal("10334.82")
 
 
 def test_finds_persons_by_name():
@@ -21,9 +25,30 @@ def test_calculates_per_minute_cost_correctly():
 
 def test_calculates_meeting_cost_correctly():
     salaries = [DOLLAR_PER_MIN_SALARY, DOLLAR_PER_MIN_SALARY, DOLLAR_PER_MIN_SALARY]
-    assert cost_calulator.calc_meeting_cost(salaries=salaries, minutes=1) == len(
-        salaries
-    )
-    assert cost_calulator.calc_meeting_cost(salaries=salaries, minutes=60) == 60 * len(
-        salaries
-    )
+    mtg_len = 1
+    expected = len(salaries)
+    actual = cost_calulator.calc_meeting_cost(salaries=salaries, minutes=mtg_len)
+    assert actual == expected
+
+    mtg_len = 60
+    expected = mtg_len * len(salaries)
+    actual = cost_calulator.calc_meeting_cost(salaries=salaries, minutes=mtg_len)
+    assert actual == expected
+
+    per_min_fifty = round(FIFTY_SALARY / MINUTES_PER_YEAR, 2)
+    salaries = [FIFTY_SALARY]
+    expected = round(mtg_len * per_min_fifty, 2)
+    actual = cost_calulator.calc_meeting_cost(salaries=salaries, minutes=mtg_len)
+    assert actual == expected
+
+    per_min_weird = round(WEIRD_SALARY / MINUTES_PER_YEAR, 2)
+    salaries = [WEIRD_SALARY]
+    expected = round(mtg_len * per_min_weird, 2)
+    actual = cost_calulator.calc_meeting_cost(salaries=salaries, minutes=mtg_len)
+    assert actual == expected
+
+    salaries = [DOLLAR_PER_MIN_SALARY, FIFTY_SALARY, WEIRD_SALARY]
+    per_min = per_min_weird + per_min_fifty + 1
+    expected = round(per_min * mtg_len, 2)
+    actual = cost_calulator.calc_meeting_cost(salaries=salaries, minutes=int(mtg_len))
+    assert actual == expected

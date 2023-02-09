264f300c8cc6b5126e92e51e22d9a46a7059c446
Bill Horvath <bhorvath@umich.edu>
2023-02-09 10:46:40 -0500

    Fix for gunicorn calling a function instead of a Flask instance.

--- src/co/deability/um_mcc/app.py
+++ src/co/deability/um_mcc/app.py
@@ -3,13 +3,8 @@ from flask import Flask
 
 from co.deability.um_mcc import init_app
 
-
-def create_app():
-    logging.getLogger().info("Starting UM_MCC API...")
-    app: Flask = init_app()
-    return app
-
+logging.getLogger().info("Starting UM_MCC API...")
+app: Flask = init_app()
 
 if __name__ == "__main__":
-    app: Flask = create_app()
     app.run()

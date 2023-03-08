f5f76ae25a711e806f218eb0f2317cbfa4665c0f
Bill Horvath <bhorvath@umich.edu>
2023-03-08 18:05:32 -0500

    Specified the port as 8000 when the API is started manually.

--- src/co/deability/um_mcc/app.py
+++ src/co/deability/um_mcc/app.py
@@ -1,10 +1,12 @@
 import logging
+
 from flask import Flask
 
 from co.deability.um_mcc import init_app
+
 
 logging.getLogger().info("Starting UM_MCC API...")
 app: Flask = init_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8000)

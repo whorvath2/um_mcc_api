5eae4ce5a9d41c407330f37001ffd2d1110a6f1e
Bill Horvath <bhorvath@umich.edu>
2023-03-22 16:19:21 -0400

    Removing vercel-specific code for now.

--- vercel_build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-python3 -m venv .venv
-source .venv/bin/activate
-.venv/bin/python3 -m pip install --upgrade pip
-.venv/bin/pip install setuptools
-.venv/bin/pip install build
-.venv/bin/python3 -m build
